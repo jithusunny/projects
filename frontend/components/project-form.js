02005ad83957a8fa691c84d03d905cf83d875401
Jithu Sunny <jithusunnyk@gmail.com>
2025-06-29 21:13:56 +0530

    update project form style

--- frontend/components/project-form.js
+++ frontend/components/project-form.js
@@ -1,4 +1,5 @@
 import { LitElement, html, css } from 'lit';
+import { sharedStyles } from '../styles/shared-styles.js';
 import '@shoelace-style/shoelace/dist/components/dialog/dialog.js';
 import '@shoelace-style/shoelace/dist/components/input/input.js';
 import '@shoelace-style/shoelace/dist/components/textarea/textarea.js';
@@ -7,12 +8,31 @@ import { createProject, updateProject } from '../services/projects.js';
 
 export class ProjectForm extends LitElement {
   static properties = {
-    open: { type: Boolean, reflect: true },
+    open: { type: Boolean },
     loading: { type: Boolean },
     editProject: { type: Object },
   };
 
-  static styles = css``;
+  static styles = [sharedStyles, css`
+    sl-dialog {
+      --width: 400px;
+    }
+    sl-dialog::part(body) {
+      display: flex;
+      flex-direction: column;
+      gap: var(--space-4);
+      padding-block: var(--space-3);
+    }
+    sl-input::part(form-control-label),
+    sl-textarea::part(form-control-label) {
+      font-size: var(--font-size-small);
+      color: var(--grey-700);
+    }
+    sl-input::part(input),
+    sl-textarea::part(textarea) {
+      font-size: var(--font-size-small);
+    }
+  `];
 
   constructor() {
     super();
@@ -25,10 +45,24 @@ export class ProjectForm extends LitElement {
     const isEdit = !!this.editProject;
     return html`
       <sl-dialog ?open=${this.open} label=${isEdit ? 'Edit Project' : 'New Project'} @sl-request-close=${this._onClose}>
-        <sl-input name="name" label="Name" required .value=${this.editProject?.name || ''}></sl-input>
-        <sl-textarea name="description" label="Description" rows="3" .value=${this.editProject?.description || ''}></sl-textarea>
+        <sl-input 
+          name="name" 
+          label="Name" 
+          required 
+          .value=${this.editProject?.name || ''}
+          placeholder="Enter project name"
+        ></sl-input>
+        <sl-textarea 
+          name="description" 
+          label="Description" 
+          rows="4" 
+          .value=${this.editProject?.description || ''}
+          placeholder="Enter project description"
+        ></sl-textarea>
 
-        <sl-button slot="footer" variant="primary" ?loading=${this.loading} @click=${this._submit}>${isEdit ? 'Save' : 'Create'}</sl-button>
+        <sl-button slot="footer" variant="primary" ?loading=${this.loading} @click=${this._submit}>
+          ${isEdit ? 'Save' : 'Create'}
+        </sl-button>
       </sl-dialog>`;
   }
 
